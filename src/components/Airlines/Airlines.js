13467b9bfbada7e6d1fb33fa61fb471ba0402669
lev <lleevv2020@gmail.com>
2023-11-30 14:16:33 +0400

    add

--- src/components/Airlines/Airlines.js
+++ src/components/Airlines/Airlines.js
@@ -1,16 +1,34 @@
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+
 import Airline from '../Airline'
 import Button from '../Button'
 
 import classes from './Airlines.module.scss'
 
+const TICKETS_PER_PAGE = 5
+const SHOW_TICKETS_PER_CLICK = 5
+
 export default function Airlines() {
+  const [showedTicketsAmount, setShowedTicketsAmount] = useState(TICKETS_PER_PAGE)
+  const allTickets = useSelector((state) => state.flights.currentTickets)
+  const tickets = allTickets.slice(0, showedTicketsAmount)
   return (
     <section className={classes['airlines']}>
-      <Airline className={classes['airlines__item']} key={'ticket.id1'} />
-      <Airline className={classes['airlines__item']} key={'ticket.id2'} />
-      <Airline className={classes['airlines__item']} key={'ticket.id3'} />
+      {tickets.length === 0 ? (
+        <p className={classes['airlines__no-flights']}>Рейсов, подходящих под заданные фильтры, не найдено.</p>
+      ) : (
+        <ul className={classes['airlines__list']}>
+          {tickets.map((ticket) => (
+            <Airline className={classes['airlines__item']} ticket={ticket} key={ticket.id} />
+          ))}
+        </ul>
+      )}
 
-      <Button onClick={() => {}} isShown={true} />
+      <Button
+        onClick={() => setShowedTicketsAmount((prev) => prev + SHOW_TICKETS_PER_CLICK)}
+        isShown={tickets.length ? true : false}
+      />
     </section>
   )
 }
