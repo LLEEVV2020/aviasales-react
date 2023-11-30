8e606d729f3e22d35771f1d68277e0bbd7070514
lev <lleevv2020@gmail.com>
2023-11-30 14:51:37 +0400

    add

--- src/components/Airline/Airline.js
+++ src/components/Airline/Airline.js
@@ -1,12 +1,30 @@
-//import { Flight } from '../Flight/Flight'
+import { Flight } from '../Flight/Flight'
+
 import classes from './Airline.module.scss'
 import companyLogo from './images/company-logo.png'
 
-export default function Airline({ className }) {
+function getFormattedPrice(price) {
+  const thousands = Math.floor(price / 1000)
+  const rest = price % 1000
+  let formattedPrice = ''
+
+  if (thousands) formattedPrice += thousands
+
+  if (rest === 0) formattedPrice += ' 000'
+  else if (rest < 10) formattedPrice += ` 00${rest}`
+  else if (rest < 100) formattedPrice += ` 0${rest}`
+  else formattedPrice += ` ${rest}`
+
+  return formattedPrice
+}
+
+export default function Airline({ className, ticket }) {
+  const formattedPrice = getFormattedPrice(ticket.price)
+
   return (
     <li className={`${className} ${classes['airline']}`}>
       <div className={classes['airline__header']}>
-        <div className={classes['airline__price']}>formattedPrice &#8381;</div>
+        <div className={classes['airline__price']}>{formattedPrice} &#8381;</div>
         <div className={classes['airline__company-logo']}>
           <img
             className={classes['airline__logo-image']}
@@ -18,9 +36,12 @@ export default function Airline({ className }) {
         </div>
       </div>
       <div className={classes['airline__body']}>
-        <ul className={classes['airline__flights-list']}>Flight</ul>
+        <ul className={classes['airline__flights-list']}>
+          {ticket.segments.map((segment) => (
+            <Flight className={classes['airline__flights-item']} segment={segment} key={segment.date} />
+          ))}
+        </ul>
       </div>
     </li>
   )
 }
-//<Flight className={classes['airline__flights-item']} segment={segment} key={segment.date} />
